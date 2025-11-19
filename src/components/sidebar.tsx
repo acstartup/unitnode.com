5b437ff69790e5dfe35dd26e47f79a0673d20033
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2025-11-19 17:35:24 -0500

    (feat): base sidebar in components

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,7 @@
 export default function Sidebar() {
     return (
-        <div className="flex flex-row">
-                <div className="basis-sm">Sidebar</div>
-        </div>
+        <aside className="h-screen w-80 bg-gray-100 text-white p-4">
+            UnitNode Sidebar
+        </aside>
     )
 }
