f3767f027cb598623310cdb648685264fc4568bf
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2025-11-19 21:47:15 -0500

    (feat): basic sidebar with UnitNode as sidebar header, with properties and settings navigation and border

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,22 @@
 export default function Sidebar() {
     return (
-        <aside className="h-screen w-80 bg-gray-100 text-white p-4">
-            UnitNode Sidebar
+        <aside className="h-screen w-64 bg-white border-r border-gray-400 text-black flex flex-col">
+            {/* Sidebar header */}
+            { /* logo area */}
+            {/* "div" because it is boxes (less critical) than critical side ("aside") information*/}
+            <div className ="px-6 py-5">
+                <h1 className="text-xl font-semibold text-gray-900">UnitNode</h1>
+            </div>
+
+            {/* Navigation (pages) */}
+            <nav className="flex-1 px-4 py-6 space-y-1">
+                <a href="#" className="flex items-center px-3 py-2 text-sm font-medium text-gray-900 rounded-md hover:bg-gray-100">
+                    Properties
+                </a>
+                <a href="#" className="flex items-center px-3 py-2 text-sm font-medium text-gray-900 rounded-sm hover:bg-gray-100">
+                    Settings
+                </a>
+            </nav>
         </aside>
     )
 }
