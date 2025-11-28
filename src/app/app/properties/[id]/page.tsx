9a29655ffecfd0b0871437cd376317a16807346c
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2025-11-28 17:58:56 -0500

    (feat): Page header

--- src/app/app/properties/[id]/page.tsx
+++ src/app/app/properties/[id]/page.tsx
@@ -3,7 +3,9 @@
 export default function PropertyDetailsPage() {
     return (
         <div className="w-full bg-white">
-            {/* blank for now */}
+            <div className="mb-1">
+                <h1 className="text-3xl font-semibold text-gray-900 px-8 py-12">Property details</h1>
+            </div>
         </div>
     )
 }
