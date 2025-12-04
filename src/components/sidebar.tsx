c5950c829e6271efbe82bc847a78521f122e2ae4
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2025-12-04 14:53:52 -0500

    (feat): dropdown arrow

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -11,12 +11,25 @@ export default function Sidebar() {
         <aside className="h-screen w-60 bg-white border-r border-gray-400 text-black flex flex-col">
             {/* Sidebar header */}
                 <div className="flex items-center gap px-2 border-gray-200">
-                    <button className="flex hover:bg-gray-200 items-center mx-1 my-2 px-2 py-2 gap-3 rounded-lg w-full items-center">
+                    <button className="flex hover:bg-gray-100 items-center mx-1 my-3 px-2 py-1.5 gap-3 rounded-lg w-full items-center">
                         { /* logo area */}
                         <div className="w-6 h-6 bg-gray-100 rounded-sm flex items-center justify-center">
                             <span className="text-black ont-bold text-xs">UN</span>
                         </div>
                         <span className="flex font-medium text-sm text-gray-700 flex-1">UnitNode</span>
+                        <svg
+                            className="h-4 w-4 text-gray-600 pointer-events-none"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M6 9l6 6 6-6"
+                            />
+                        </svg>
                     </button>
                 </div>
 
